bdd95949df7c804e1c783a24e6be78b428be3c6a
Darryl Balderas <drrylbalderas@gmail.com>
2024-02-15 20:39:27 -0800

    Create an http server

--- kind_app/main.go
+++ kind_app/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Hello world from %s in %s deployed in %s", os.Getenv("PODNAME"), os.Getenv("PODNAMESPACE"), os.Getenv("NODENAME"))
+}
+
 func main() {
-	log.Printf("Hello world from %s", os.Getenv("PODNAME"))
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
